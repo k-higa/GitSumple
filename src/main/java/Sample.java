a3e77ae7ce842718ae4ac3ee79eae0b2f713c5c0
比嘉　和之 <w-_-waaa1@i.softbank.jp>
2017-03-30 23:57:35 +0900

    commit

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -6,5 +6,6 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("sample!");
         System.out.println("develop");
+        System.out.println("return");
     }
 }
